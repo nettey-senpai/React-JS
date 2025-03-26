f6e99390b81d5e9bab4ad463dce05d7ca605aac3
Jeffrey <netteyjeff@gmail.com>
2025-03-26 15:07:23 +0000

    Styling exercise

--- 01. Fundamentals/01. Components/react-demos/src/App.jsx
+++ 01. Fundamentals/01. Components/react-demos/src/App.jsx
@@ -1,12 +1,16 @@
-import "./index.css";
+// import "./index.css";
 import { FaComputer } from "react-icons/fa6";
 import { FaCartArrowDown } from "react-icons/fa";
+import StyleCard from "./components/StyleCard";
+import ProfileCard from "./components/ProfileCard";
+import IconComponent from "./components/IconComponent";
 
 const App = () => {
   return (
     <section>
-      <FaCartArrowDown />
-      <FaComputer />
+      <StyleCard />
+      <ProfileCard />
+      <IconComponent />
     </section>
   );
 };
---
+++ 01. Fundamentals/01. Components/react-demos/src/components/ProfileCard.jsx
@@ -0,0 +1,21 @@
+const ProfileCard = () => {
+  const styles = {
+    backgroundColor: "lightgray",
+    padding: "15px",
+    borderRadius: "8px",
+    color: "black",
+  };
+  return (
+    <div style={styles}>
+      <h1>Hello User!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex dolorem
+        consectetur necessitatibus iusto perspiciatis consequatur corporis culpa
+        nobis voluptatibus illo! Atque assumenda, veritatis laborum excepturi
+        dolor eum consectetur ad qui?
+      </p>
+    </div>
+  );
+};
+
+export default ProfileCard;
