1408250ddf7030e0e7559d718fc8c3a4a9a1bb25
Jeffrey <netteyjeff@gmail.com>
2025-03-26 12:56:29 +0000

    props

--- 01. Fundamentals/01. Components/react-demos/src/App.jsx
+++ 01. Fundamentals/01. Components/react-demos/src/App.jsx
@@ -1,12 +1,22 @@
-import ProductList from "./components/ProductList";
-import UserList from "./components/UserList";
-
 const App = () => {
   return (
-    <div>
-      {/* <UserList /> */}
-      <ProductList />
-    </div>
+    <User
+      name="Jeff"
+      age={25}
+      isMarried={false}
+      hobbies={["Coding", "Music", "Sleeping"]}
+    />
+  );
+};
+
+const User = ({ name, age, isMarried, hobbies }) => {
+  return (
+    <section>
+      <h1>Name: {name}</h1>
+      <h2>Age: {age}</h2>
+      <h3>Is Married: {isMarried}</h3>
+      <h4>Hobbies: {hobbies[0]}</h4>
+    </section>
   );
 };
 
