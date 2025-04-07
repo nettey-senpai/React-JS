3c3eec7dd43003a870ea9fe27c34622f72140031
Jeffrey <netteyjeff@gmail.com>
2025-04-07 12:24:46 +0000

    Usestate exercise on child components

--- 01. Fundamentals/01. Components/react-demos/src/App.jsx
+++ 01. Fundamentals/01. Components/react-demos/src/App.jsx
@@ -1,24 +1,14 @@
 import { useState } from "react";
+import ComponentOne from "./components/ComponentOne";
+import ComponentTwo from "./components/ComponentTwo";
 
 const App = () => {
-  const [movies, setMovies] = useState([
-    { id: 1, title: "Dune Part One", ratings: 8 },
-    { id: 2, title: "Dune Part Two", ratings: 8.5 },
-  ]);
-
-  const handleClick = () => {
-    setMovies(
-      movies.map((m) => (m.id == 1 ? { ...movies, title: "Dark Knight" } : m))
-    );
-  };
+  const [count, setCount] = useState(0);
 
   return (
     <section>
-      {movies.map((m) => (
-        <li key={Math.random()}>{m.title}</li>
-      ))}
-
-      <button onClick={handleClick}>Change Name</button>
+      <ComponentOne count={count} onClickHandler={() => setCount(count + 1)} />
+      <ComponentTwo count={count} onClickHandler={() => setCount(count + 1)} />
     </section>
   );
 };
