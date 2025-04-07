e2862a0dcb8deba3a132e22ad3bbd569556e3a6a
Jeffrey <netteyjeff@gmail.com>
2025-04-07 11:15:53 +0000

    Events exercise

--- 01. Fundamentals/01. Components/react-demos/src/App.jsx
+++ 01. Fundamentals/01. Components/react-demos/src/App.jsx
@@ -1,16 +1,40 @@
-// import "./index.css";
-import { FaComputer } from "react-icons/fa6";
-import { FaCartArrowDown } from "react-icons/fa";
-import StyleCard from "./components/StyleCard";
-import ProfileCard from "./components/ProfileCard";
-import IconComponent from "./components/IconComponent";
+const Move = () => {
+  const moveHandler = () => {
+    alert("Mouse move event fired");
+    console.log("Mouse move event fired");
+  };
+  return (
+    <p onMouseMove={moveHandler}>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est, deleniti
+      esse ducimus nobis dolorem itaque eum suscipit. Quibusdam, beatae.
+      Consequuntur?
+    </p>
+  );
+};
+
+const Copy = () => {
+  const copyHandler = () => console.log("Stop copying my shi");
+
+  return (
+    <p onCopy={copyHandler}>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate,
+      repellendus! Soluta natus laudantium rerum quos nulla labore. Quas,
+      adipisci eligendi!
+    </p>
+  );
+};
+
+const Button = () => {
+  const handleClick = () => console.log(Math.round(Math.random() * 10));
+  return <button onClick={handleClick}>Click</button>;
+};
 
 const App = () => {
   return (
     <section>
-      <StyleCard />
-      <ProfileCard />
-      <IconComponent />
+      <Button />
+      <Copy />
+      <Move />
     </section>
   );
 };
