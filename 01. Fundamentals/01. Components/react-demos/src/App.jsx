f8c63549f91e25ec9d2f2a2cffc3747c0750d940
Jeffrey <netteyjeff@gmail.com>
2025-04-07 12:50:24 +0000

    Usestate exercise with arrow functions

--- 01. Fundamentals/01. Components/react-demos/src/App.jsx
+++ 01. Fundamentals/01. Components/react-demos/src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
-import ComponentOne from "./components/ComponentOne";
-import ComponentTwo from "./components/ComponentTwo";
+import ExampleOne from "./components/ExampleOne";
+import ExampleTwo from "./components/ExampleTwo";
+import ExampleThree from "./components/ExampleThree";
+
 
 const App = () => {
-  const [count, setCount] = useState(0);
 
   return (
-    <section>
-      <ComponentOne count={count} onClickHandler={() => setCount(count + 1)} />
-      <ComponentTwo count={count} onClickHandler={() => setCount(count + 1)} />
-    </section>
+    <div>
+      <ExampleOne />
+      <ExampleTwo />
+      <ExampleThree />
+    </div>
   );
 };
 
