6090d3caa3d8ce5d5f088f91f1a2d062abffa0f9
Jeffrey <netteyjeff@gmail.com>
2025-04-08 09:57:11 +0000

    Usestate challenge

--- 01. Fundamentals/01. Components/react-demos/src/App.jsx
+++ 01. Fundamentals/01. Components/react-demos/src/App.jsx
@@ -1,5 +1,6 @@
 import Counter from "./components/Counter";
 import Profile from "./components/Profile";
+import ShoppingList from "./components/ShoppingList";
 import TodoList from "./components/TodoList";
 
 
@@ -8,9 +9,10 @@ const App = () => {
 
   return (
     <div>
-      {/* <Counter /> */}
-      {/* <TodoList /> */}
+      <Counter />
+      <TodoList />
       <Profile />
+      <ShoppingList />
     </div>
   );
 };
