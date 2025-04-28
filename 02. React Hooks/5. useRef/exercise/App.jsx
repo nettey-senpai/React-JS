2aa53a6143943d6c313f01b7aed444aa639d3bbf
Jeffrey <netteyjeff@gmail.com>
2025-04-28 12:50:52 +0000

    useRef hook exercise

---
+++ 02. React Hooks/5. useRef/exercise/App.jsx
@@ -0,0 +1,13 @@
+import FocusInput from "./components/FocusInput"
+import Timer from "./components/Timer"
+
+const App = () => {
+  return (
+    <div>
+        <FocusInput />
+        <Timer />
+    </div>
+  )
+}
+
+export default App
