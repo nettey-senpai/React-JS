1423e969893c6b2c8de1b62b5b9ba576c1285a5a
Jeffrey <netteyjeff@gmail.com>
2025-05-05 14:59:29 +0000

    Custom hooks useId hook

---
+++ 02. React Hooks/6. Custom Hooks/live coding/useId/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import UniqueID from "./components/UniqueID";
+
+const App = () => {
+  return (
+    <div>
+      <UniqueID />
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus
+        aspernatur eos nobis dolorum ratione, fugit reiciendis autem deleniti
+        laudantium nam reprehenderit quibusdam, voluptas sed unde repudiandae
+        inventore pariatur facere consectetur.
+      </p>
+      <UniqueID />
+    </div>
+  );
+};
+
+export default App;
